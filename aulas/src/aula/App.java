49bfd3ff8db20c16232fa5410536771555311b5e
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-09-03 12:27:50 -0300

    oiup

---
+++ aulas/src/aula/App.java
@@ -0,0 +1,15 @@
+package aula;
+
+public class App{
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+void dadosLivros(){
+    System.out.println("nome do livro" + nome);
+    System.out.println("preço" + preco);
+}
+}
--- mobile/app.java
+++
@@ -1,8 +0,0 @@
-public class app {
-
-public static void main(String[] args) {
-    app livro = new app();
-;
-livro.nome = "O senhor dos aneis";
-livro.descricao = "Uma obra lieteraria de fantasia";
-livro.isbn = "9783"
