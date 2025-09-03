f9922a1c055f8f6075d3a1168f9d45184c8813a0
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-09-03 11:47:04 -0300

    oid

---
+++ mobile/app.java
@@ -0,0 +1,8 @@
+public class app {
+
+public static void main(String[] args) {
+    app livro = new app();
+;
+livro.nome = "O senhor dos aneis";
+livro.descricao = "Uma obra lieteraria de fantasia";
+livro.isbn = "9783"
