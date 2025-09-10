f7d87eea253e8e53a8314d83571390b18efc1555
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-09-10 12:22:17 -0300

    ooii

--- aulas/src/aula/App.java
+++ aulas/src/aula/App.java
@@ -1,6 +1,7 @@
 package aula;
 
 public class App{
+    
 String nome;
 String descricao;
 String autor;
@@ -11,5 +12,6 @@ String datapub;
 void dadosLivros(){
     System.out.println("nome do livro" + nome);
     System.out.println("preço" + preco);
+    System.out.println("descrição" + descricao);
 }
 }
---
+++ aulas/src/aula/App3.java
@@ -0,0 +1,7 @@
+package aula;
+
+public class App3 {
+    public static void elevar() {
+        System.out.println("esse método não calcula nada");
+    }
+}
